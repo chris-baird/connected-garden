22da6ed64ec52af2981ed416999090c474685610
Chris Baird <cab3953@gmail.com>
2023-07-20 11:53:04 -0700

    Set up file structure, created simple outline of program with psudocode

---
+++ app.js
@@ -0,0 +1,7 @@
+// App starts
+// Startup functions runs
+// Main loop starts
+// Water level is checked
+// Moisture level is checked
+// Pump is run if moisture level too low
+// Pump turns off if moisture is good
